01adaf0b34c01132ffc7064beb310724b880039b
JAlexander213 <jovan.diaz.roj@gmail.com>
2025-10-18 16:59:54 -0600

    Correccion server

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,24 @@ use actix_web::{web, App, HttpServer};
 use dotenvy::dotenv;
 use sqlx::{MySql, Pool};
 use std::env;
-use actix_web::web::route;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
+    // Conexión a la base de datos
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL no configurada");
     let pool = Pool::<MySql>::connect(&database_url)
         .await
         .expect("Error al conectar a la base de datos");
 
-    println!("Servidor corriendo en http://127.0.0.1:8080/");
+    // Obtener el puerto desde Render (o usar 8080 localmente)
+    let port: u16 = env::var("PORT")
+        .unwrap_or_else(|_| "8080".to_string())
+        .parse()
+        .expect("PORT debe ser un número");
+
+    println!("Servidor corriendo en http://0.0.0.0:{}/", port);
 
     HttpServer::new(move || {
         let cors = Cors::default()
@@ -34,7 +40,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(pool.clone()))
             .configure(controller::routes)
     })
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    .bind(("0.0.0.0", port))? // <- Muy importante para Render
+    .run()
+    .await
 }
